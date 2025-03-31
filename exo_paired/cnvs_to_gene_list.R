be1c72da282c96bdf46f97ef3018d479468f5524
hophee <zaycev.i21@gmail.com>
2025-04-01 00:34:04 +0300

    CNV clear

--- exo_paired/cnvs_to_gene_list.R
+++ exo_paired/cnvs_to_gene_list.R
@@ -3,7 +3,7 @@ library(GenomicRanges)
 library(TxDb.Hsapiens.UCSC.hg38.knownGene)
 library(org.Hs.eg.db)
 
-data <- read_delim('tumor_with_rg.call.cns') %>% filter(abs(log2) > 0.5)
+data <- read_delim('tumor_with_rg.call.cns') %>% mutate(width = end-start, effect = width* abs(log2)) %>% filter(chromosome != 'chrY')
 
 gr <- GRanges(seqnames=data$chromosome, ranges=IRanges(start=data$start, end=data$end), log2=data$log2)
 
@@ -17,4 +17,9 @@ annotated$gene <- mapIds(org.Hs.eg.db,
                            column="SYMBOL",
                            keytype="ENTREZID")
 
-annotated %>% pull(gene) %>% unique() %>% write(file='high_cnv_gene_list.txt')
+hard_filtered <- annotated %>% filter(effect > quantile(unique(effect), .95))
+annotated %>% pull(gene) %>% unique() %>% sort() %>% write(file='very_high_cnv_gene_list.txt')
+
+ggplot() +
+  geom_point(data=data, aes(x=effect, y=weight))
+annotated %>% filter(chromosome == 'chrX') %>% pull(gene) %>% write(file='chrX_list.txt')
