4ded59191777881304aa3b347dfeca230bad0d12
hophee <zaycev.i21@gmail.com>
2025-07-29 15:32:26 +0300

    new ids

--- multioms/get_ids.R
+++ multioms/get_ids.R
@@ -7,8 +7,8 @@ get_id_of_surname <- function(fam, nk_num){
   ids <- meta %>% filter(familia %in% fam) %>% filter(nukleinovaa_kislota==nk[nk_num]) %>% pull(1)
   return(c(paste0(ids,'_1.fq.gz'), paste0(ids,'_2.fq.gz')))
 }
-meta %>% pull(2) %>% unique() %>% head(n=4) %>% get_id_of_surname(.,nk_num=2) %>% write(file='somatic_hn4.txt')
-meta %>% pull(2) %>% unique() %>% head(n=4) %>% get_id_of_surname(.,nk_num=3) %>% write(file='germ_hn4.txt')
+meta %>% pull(2) %>% unique() %>% tail(n=4) %>% get_id_of_surname(.,nk_num=2) %>% write(file='somatic_tn4.txt')
+meta %>% pull(2) %>% unique() %>% tail(n=4) %>% get_id_of_surname(.,nk_num=3) %>% write(file='germ_tn4.txt')
 
-f4 <- meta %>% pull(2) %>% unique() %>% head(n=4)
+f4 <- meta %>% pull(2) %>% unique() %>% tail(n=4)
 meta %>% filter(familia %in% f4) %>% filter(nukleinovaa_kislota %in% c("Соматическая ДНК","Герминальная ДНК")) %>% select(1, 2, 4)
