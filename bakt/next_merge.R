5d74cbfb57eb3f58f5d1dcf967297595697ad3d0
hophee <zaycev.i21@gmail.com>
2025-08-25 01:30:03 +0300

    Pics must be merged

---
+++ bakt/next_merge.R
@@ -0,0 +1,119 @@
+library(ggbio)
+library(GenomicRanges)
+library(dplyr)
+
+# Преобразуем данные в GRanges
+narna_gr <- makeGRangesFromDataFrame(
+  narna_vis,
+  seqnames.field = "number_sequence_id",
+  start.field = "start",
+  end.field = "end",
+  strand.field = "strand"
+)
+
+
+# Добавляем cluster_size как метаданные
+mcols(narna_gr)$cluster_size <- narna_vis$cluster_size
+
+genome_circle <- GRanges(
+  seqnames = "contig_1",  # или ваше имя последовательности
+  IRanges(start = 1, end = max(eczv$stop)+100),
+  strand = "*"
+)
+# Создаем отдельные объекты для каждого strand
+plus_gr <- narna_gr[strand(narna_gr) == "+"]
+minus_gr <- narna_gr[strand(narna_gr) == "-"]
+
+
+sig_gr <- full_tab %>% filter(significant=='yes') %>% filter(!is.na(gene.x))%>% 
+  makeGRangesFromDataFrame(. ,
+                           seqnames.field = "gene.x",
+                           start.field = "start.x",
+                           end.field = "stop.x",
+                           strand.field = "strand.x")
+
+
+# Создаем график с разными цветами для каждого strand
+p <- ggbio()+
+  circle(plus_gr, geom='rect', aes(fill=strand, col=strand))+
+  circle(minus_gr, geom='rect', aes(fill=strand, col=strand))+
+  circle(plus_gr, geom='text', aes(label=cluster_size, vjust = 12))+
+  circle(minus_gr, geom='text', aes(label=cluster_size, vjust = -0.25))+
+  
+  circle(genome_circle, geom = "rect", 
+         aes(fill = "background"), 
+         fill = "lightgray", alpha = 0.01, size = 0.5)+
+  labs(fill='Strand of naRNA4', title='Расположение naRNA4 по геному')+
+  theme(plot.title = element_text(hjust = 0.5))+
+  guides(col=F)
+
+
+# Добавляем надписи отдельно
+print(p)
+
+
+
+#-----------------------
+bin_size <- 10000
+chrom_length <- max(eczv$stop)
+bins <- tileGenome(seqlengths = c(contig_1 = chrom_length), tilewidth = bin_size, cut.last.tile.in.chrom = TRUE)
+mcols(bins)$density <- countOverlaps(bins, narna_gr)
+genome_circle <- GRanges(seqnames = "contig_1", IRanges(start = 1, end = chrom_length))
+p_density <- ggbio() +
+  circle(genome_circle, geom = "rect",
+         fill = "lightgray", alpha = 0.1, size = 0.5) +
+  circle(bins, geom = "rect", aes(fill = density), color = NA) +
+  scale_fill_gradient(low = "white", high = "steelblue") +
+  labs(title = "Карта плотности naRNA4 по геному", fill='Число генов\nв окне 10кб') +
+  theme(plot.title = element_text(hjust = 0.5))
+
+print(p_density)
+
+#-----------
+
+min_dist <- function(x1, x2, y1, y2){
+  dists1 <- x1 - y1
+  dists2 <- x2 - y2
+  closest_idx1 <- which.min(abs(dists1))
+  closest_idx2 <- which.min(abs(dists2))
+  if (abs(dists1[closest_idx1])<abs(dists2[closest_idx2])){
+    return(dists1[closest_idx1])
+  }else{
+    return(dists2[closest_idx2])
+  }
+}
+min_dist2 <- function(x1, x2, y1, y2, df1, df2){
+  dists1 <- x1 - y1
+  dists2 <- x2 - y2
+  closest_idx1 <- which.min(abs(dists1))
+  closest_idx2 <- which.min(abs(dists2))
+  if (abs(dists1[closest_idx1])<abs(dists2[closest_idx2])){
+    min_res <- dists1[closest_idx1]
+    idx <- closest_idx1
+    }else{
+    min_res <- dists2[closest_idx2]
+    idx <- closest_idx2
+    }
+  lct <- df1$locus_tag[closest_idx2]
+  cl_sz <- df2$cluster_size[which(df2$locus_tag==lct)]
+  return(c(min_res, cl_sz))
+}
+narna_plus <- full_tab %>% filter((gene.x=='naRNA4') & (strand.x=='+'))
+narna_minus <- full_tab %>% filter((gene.x=='naRNA4') & (strand.x=='-'))
+sig_plus <- full_tab %>% filter((strand.x=='+') & (significant=='yes')) %>% 
+  mutate(rna_dist=mapply(function(x1, x2){min_dist2(x1, x2, narna_plus$stop.x, narna_plus$start.x, narna_plus, cl_sz_plus_strand)[1]}, start.x, stop.x)) %>% 
+  mutate(cl_sz=mapply(function(x1, x2){min_dist2(x1, x2, narna_plus$stop.x, narna_plus$start.x, narna_plus, cl_sz_plus_strand)[2]}, start.x, stop.x))
+sig_minus <- full_tab %>% filter((strand.x=='-') & (significant=='yes')) %>% 
+  mutate(rna_dist=mapply(function(x1, x2){min_dist2(x1, x2, narna_minus$stop.x, narna_minus$start.x, narna_minus, cl_sz_minus_strand)[1]}, start.x, stop.x)) %>% 
+  mutate(cl_sz=mapply(function(x1, x2){min_dist2(x1, x2, narna_minus$stop.x, narna_minus$start.x, narna_minus, cl_sz_minus_strand)[2]}, start.x, stop.x))
+
+
+rbind(sig_plus, sig_minus) %>% mutate(log2fold_change=as.numeric(log2fold_change)) %>% 
+  ggplot()+
+  geom_point(aes(y=log2fold_change, x=log10(abs(rna_dist)), col=cl_sz))+
+  scale_color_gradient(low='blue', high='red')+
+  labs(title = 'Отношение расстояния от ближайшего naRNA4 к log2(FC)', color='Размер \nближайшего \nкластера', 
+       x='log10(|Расстояние до naRNA4|)', y='log2(FC)')+
+  theme_minimal()+
+  theme(plot.title = element_text(hjust=0.5))
+
--- bakt/rna_clust.R
+++ bakt/rna_clust.R
@@ -47,10 +47,80 @@ rep(rle2$values, rle2$lengths/rle2$values) %>%
   ggtitle('Cluster size distribution on the minus strand')+
   xlab('Cluster size')
 
-for_vis_df <- bind_rows(cl_sz_plus_strand, cl_sz_minus_strand) %>% select(-grep('*\\.y', colnames(cl_sz_minus_strand)))
-colnames(for_vis_df) <- sub('\\.x', '', colnames(cl_sz_minus_strand))
+for_vis_df <- bind_rows(cl_sz_plus_strand, cl_sz_minus_strand) %>% 
+  select(-grep('*\\.y', colnames(cl_sz_minus_strand))) %>% dplyr::rename('end'='stop.x')
+colnames(for_vis_df) <- sub('\\.x', '', colnames(for_vis_df))
+narna_vis <- for_vis_df %>% filter(gene=='naRNA4') %>% select(1, 3, 4, 5, 17)
+
+plus_vis <- cl_sz_plus_strand %>% select(-grep('*\\.y', colnames(cl_sz_plus_strand))) %>% dplyr::rename('end'='stop.x') %>% filter(gene.x=='naRNA4') %>%  select(1, 3, 4, 5, 17)
+colnames(plus_vis) <- sub('\\.x', '', colnames(plus_vis))
+
+min_vis <- cl_sz_minus_strand %>% select(-grep('*\\.y', colnames(cl_sz_minus_strand))) %>% dplyr::rename('end'='stop.x') %>% filter(gene.x=='naRNA4') %>%  select(1, 3, 4, 5, 17)
+colnames(min_vis) <- sub('\\.x', '', colnames(min_vis))
 
 chromosome.size <- max(eczv$stop)
 circos.par(cell.padding = c(0, 0, 0, 0))  # Убрать отступы
-circos.initialize("chr1", xlim = c(0, chromosome.size))
+circos.initialize("contig_1", xlim = c(0, chromosome.size))
+
+strand_colors <- c("+" = "darkgreen", "-" = "darkred")
+
+circos.track(
+  ylim = c(0, 1),
+  track.height = 0.3,  # Высота дорожки
+  panel.fun = function(x, y) {
+    if (nrow(min_vis) == 0) return()
+    
+    for (i in 1:nrow(min_vis)) {
+      start <- min_vis$start[i]
+      end <- min_vis$end[i]
+      cluster_size <- min_vis$cluster_size[i]
+      
+      # Рисуем прямоугольник
+      circos.rect(start, 0, end, 1, 
+                  col = strand_colors["-"], border = strand_colors["-"], lwd = 1)
+      
+      
+        center_x <- (start + end) / 2
+        circos.text(center_x, 0.5, labels = as.character(cluster_size),
+                    facing = "bending.inside", niceFacing = TRUE,
+                    cex = 0.7, col = "black", font = 2)
+    }
+  }
+)
+
+circos.track(
+  ylim = c(0, 1),
+  track.height = 0.3,  # Высота дорожки
+  panel.fun = function(x, y) {
+    if (nrow(plus_vis) == 0) return()
+    
+    for (i in 1:nrow(plus_vis)) {
+      start <- plus_vis$start[i]
+      end <- plus_vis$end[i]
+      cluster_size <- plus_vis$cluster_size[i]
+      
+      # Рисуем прямоугольник
+      circos.rect(start, 0, end, 1, 
+                  col = strand_colors["+"], border = strand_colors["+"], lwd = 0.5)
+      
+      # Добавляем надпись (логика такая же как раньше)
+      show_label <- TRUE
+      if (cluster_size > 1) {
+        # Проверяем, является ли это первым в кластере
+        if (i > 1 && plus_vis$start[i] <= plus_vis$end[i-1]) {
+          show_label <- FALSE
+        }
+      }
+      
+      if (show_label) {
+        center_x <- (start + end) / 2
+        circos.text(center_x, 0.5, labels = as.character(cluster_size),
+                    facing = "bending.inside", niceFacing = TRUE,
+                    cex = 0.7, col = "black", font = 2)
+      }
+    }
+  }
+)
+circos.clear()
+
 
