84e6f7309178b2d862d94ff4238793b1dcb85926
hophee <zaycev.i21@gmail.com>
2025-09-03 00:12:58 +0300

    Final types of sosedi

--- bakt/rna_typeof_neighb.R
+++ bakt/rna_typeof_neighb.R
@@ -12,7 +12,7 @@ neighbor_of_canon <- function(nd, tab=eczv){
 eczv <- read_tsv('bakt_files/ECZV.tsv', skip=2) %>% janitor::clean_names()
 locus_tag <- eczv %>% filter(gene=='naRNA4') %>% pull(locus_tag) %>% sort()
 is_canon <- c(0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1)
-canon_tab <- data.frame(locus_tag, is_canon) %>% slice(-58)
+canon_tab <- data.frame(locus_tag, is_canon) %>% slice(-58) #в 58 строке спорный образец
 all_neighb <- left_join(eczv, canon_tab, by='locus_tag') %>% neighbor_of_canon(1, tab=.) %>% select(start, type,canon_neighb, gene, locus_tag, is_canon)
 
 non_can <- all_neighb %>% filter(type %in% c('cds', 'ncRNA')) %>% filter(canon_neighb==0) %>% pull(type) %>% as.factor()
@@ -20,4 +20,7 @@ can <- all_neighb %>% filter(type %in% c('cds', 'ncRNA')) %>% filter(canon_neigh
 contingency_table <- table(group = c(rep("non_can", length(non_can)), rep("can", length(can))),
                            value = c(non_can, can))
 chi2_test <- chisq.test(contingency_table)
-print(chi2_test)
+#Значение p-value
+print(chi2_test$p.value)
+#Наблюдаемые значения
+print(chi2_test$observed)
